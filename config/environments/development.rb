98d47ec7fbe3da160eeee4f9499a678f9a8358dd
Reid Angwin <rangwin@gmail.com>
2018-02-07 02:49:19 -0500

    Fixed devise registration email smtp issues. Added homepage and navbar.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
+    config.action_mailer.perform_deliveries = true
 
     config.cache_store = :null_store
   end
@@ -37,13 +38,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
-                            port: 587,
-    domain: ENV[“GMAIL_DOMAIN”],
-    authentication: “plain”,
-                    enable_starttls_auto: true,
-    user_name: ENV[“GMAIL_USERNAME”],
-    password: ENV[“GMAIL_PASSWORD”]
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "jeffsamuelrails@gmail.com",
+    password: "rails email address"
   }
 
   # Print deprecation notices to the Rails logger.
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -5,7 +5,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => "587",
   :domain               => 'gmail.com',
-  :user_name            => 'jeffsamuelsrails@googlemail.com',
+  :user_name            => 'jeffsamuelrails@gmail.com',
   :password             => 'rails email address',
   :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+  :enable_starttls_auto => true
+}
